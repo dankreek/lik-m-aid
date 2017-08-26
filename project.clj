41454324964d8f59f96d9ec8dab315c78dbd40a9
Justin May <may.justin@gmail.com>
2017-08-26 15:21:09 -0700

    Version 0.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dankreek/lik-m-aid "0.0.2-SNAPSHOT"
+(defproject dankreek/lik-m-aid "0.0.2"
   :description "A 2d canvas sprite library for ClojureScript"
   :url "http://github.com/dankreek/lik-m-aid"
   :license {:name "Eclipse Public License"
